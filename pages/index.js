6883d5360eec579b8e306e6702a680c56d96f5ce
Ailie McCorkindale <ailiem134@hotmail.com>
2021-07-27 03:09:33 +0200

    home page content added

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container" >
+    <div className={styles.container} >
 
-      <main className="main">
-      <h1>My Portfolio</h1>
-        <p>Main Content</p>
+      <main className={styles.main}>
+      <p className={styles.portfolio}><span className={styles.rectangle}></span>PORTFOLIO</p>
+      <h1 className={styles.title}>Ailie McCorkindale</h1>
+      <h2 className={styles.subtitle}>Web Developer</h2>
+        <button className={styles.btn}>My Projects</button>
       </main>
 
-      <footer className="footer">
-        <p>Footer</p>
-      </footer>
+      
     </div>
   )
 }
