1b9a0fd83f14a2ba997cfc23e571dac04dec4592
Ailie McCorkindale <ailiem134@hotmail.com>
2021-07-28 02:31:23 +0200

    projects page content added

---
+++ components/ProjectCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../styles/ProjectCard.module.css";
+
+const imgSize = {
+    width: "100%",
+    objectFit: "cover"
+}
+
+const ProjectCard = (props) => {
+    console.log("props", props)
+  return (
+    <div
+      key={`project_${props.project.id}`}
+      className={styles.card}
+    >
+      <h2>{props.project.title}</h2>
+      <h3>{props.project.category}</h3>
+      {props.project.img && <img src={props.project.img} style={imgSize} alt="screenshot" />}
+    <div>
+      <a href={props.project.url} target="_blank">
+        Visit Site
+      </a>
+      <a href={props.project.gitHub} target="_blank">
+        See Code
+      </a>
+    </div>
+      <p>Technologies Used: {props.project.tags.join(', ')}</p>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com',
+  },
+}
