b2de820745223f522ac6b348a5069be1938f45ea
Ailie McCorkindale <ailiem134@hotmail.com>
2021-07-30 12:54:46 +0200

    imgs changed to Image component on about page, made responsive

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  images: {
-    loader: 'cloudinary',
-    path: 'https://res.cloudinary.com',
-  },
-}
+// module.exports = {
+//   images: {
+//     loader: 'cloudinary',
+//     path: 'https://res.cloudinary.com',
+//   },
+// }
--- pages/about.js
+++ pages/about.js
@@ -1,47 +1,79 @@
 import React from "react";
-import styles from '../styles/About.module.css';
-
+import styles from "../styles/About.module.css";
+import Image from "next/image";
 
 const about = () => {
+
   return (
     <div>
       <h1 className="pageTitle">About Me</h1>
-      
+
       <div className={styles.main}>
-      <div className={styles.images}>
-      <img className={styles.profileImage} src="https://res.cloudinary.com/dih7zrall/image/upload/v1627524874/Portfolio/geneva_e7afjr.jpg" />
-      <img className={styles.profileImageExtra} src="https://res.cloudinary.com/dih7zrall/image/upload/v1627523875/Portfolio/laos_dlqego.jpg" />
-      <img className={styles.profileImageExtra} src="https://res.cloudinary.com/dih7zrall/image/upload/v1627560900/Portfolio/elephant_qxbexm.jpg" />
-      </div>
-      <div className={styles.bio}>
-      <h2 className={styles.subtitle}>
-        AILIE MCCORKINDALE, Full Stack Developer
-      </h2>
-      <p>
-        Hi, I'm Ailie. I'm a web developer based in Paris, France and I am
-        currently looking for a new challenge!
-      </p>
-      <p>
-        Fresh from taking time out to backpack in South East Asia and sharpening
-        my tech skills with Ironhack's intensive web development bootcamp, I'm
-        ready to kick start my career as a full stack developer! A lover of
-        puzzles, I’m not afraid to think outside the box, try new things and use
-        my creativity and enthusiasm to solve problems.
-      </p>
-      <p>
-        I love to learn new things and expand my horizons. Although my
-        professional path has taken a few twists and turns, I’ve never stopped
-        learning whether it was through university, evening classes or online
-        tutorials, and I’ve finally found my passion in the ever changing world
-        of web development, where it is essential to keep up with all the latest
-        developments.
-      </p>
-      <p>In my spare time I enjoy travelling, reading, yoga and making games with HTML canvas.</p>
-      <h3>Technologies</h3>
-      <p>HTML 5 | CSS3 | JavaScript | TypeScript | React | Bootstrap | MaterialUI | Node.js | Express | Handlebars | Jasmine | MongoDB | Mongoose | Git | GitHub</p>
-      <h3>Languages</h3>
-      <p>English, French, Italian</p>
-      </div>
+
+        <div className={styles.images}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/geneva.jpg"}
+              width={827}
+              height={621}
+              layout="responsive"
+            />
+          </div>
+          <div className={styles.imageContainerExtra}>
+            <Image
+              src={"/laos.jpg"}
+              width={827}
+              height={621}
+              layout="responsive"
+            />
+          </div>
+          <div className={styles.imageContainerExtra}>
+            <Image
+              src={"/elephant.jpg"}
+              width={827}
+              height={621}
+              layout="responsive"
+            />
+          </div>
+        </div>
+      
+        <div className={styles.bio}>
+          <h2 className={styles.subtitle}>
+            AILIE MCCORKINDALE, Full Stack Developer
+          </h2>
+          <p>
+            Hi, I'm Ailie. I'm a web developer based in Paris, France and I am
+            currently looking for a new challenge!
+          </p>
+          <p>
+            Fresh from taking time out to backpack in South East Asia and
+            sharpening my tech skills with Ironhack's intensive web development
+            bootcamp, I'm ready to kick start my career as a full stack
+            developer! A lover of puzzles, I’m not afraid to think outside the
+            box, try new things and use my creativity and enthusiasm to solve
+            problems.
+          </p>
+          <p>
+            I love to learn new things and expand my horizons. Although my
+            professional path has taken a few twists and turns, I’ve never
+            stopped learning whether it was through university, evening classes
+            or online tutorials, and I’ve finally found my passion in the ever
+            changing world of web development, where it is essential to keep up
+            with all the latest developments.
+          </p>
+          <p>
+            In my spare time I enjoy travelling, reading, yoga and making games
+            with HTML canvas.
+          </p>
+          <h3>Technologies</h3>
+          <p>
+            HTML 5 | CSS3 | JavaScript | TypeScript | React | Bootstrap |
+            MaterialUI | Node.js | Express | Handlebars | Jasmine | MongoDB |
+            Mongoose | Git | GitHub
+          </p>
+          <h3>Languages</h3>
+          <p>English, French, Italian</p>
+        </div>
       </div>
     </div>
   );
