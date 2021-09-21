d064a0b86ef594dab40d22f9d6b036d93ee5d668
Ailie McCorkindale <ailiem134@hotmail.com>
2021-09-21 12:29:43 +0200

    use effect dependency array updated

--- pages/mini-projects/currency-converter.js
+++ pages/mini-projects/currency-converter.js
@@ -5,8 +5,8 @@ import { fetchData } from "../api/currencyConverterAPI";
 
 const CurrencyConverter = (props) => {
   const [currencies, setCurrencies] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState("EUR");
-  const [toCurrency, setToCurrency] = useState("GBP");
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
   const [amount, setAmount] = useState(1);
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
   const [exchangeRate, setExchangeRate] = useState(null);
@@ -14,6 +14,7 @@ const CurrencyConverter = (props) => {
   let toAmount = 0;
   let fromAmount = 0;
 
+  //When the amount of either currency input field changes, update the amount in the other currency input field
   if (amount) {
     if (amountInFromCurrency) {
       fromAmount = amount;
@@ -24,19 +25,21 @@ const CurrencyConverter = (props) => {
     }
   }
 
+  const { base, rates } = props.data;
+
   //Use Data from API call to populate list of currencies, and set initial values for currencies and exchange rate
   useEffect(() => {
-    const currencyList = [props.data.base, ...Object.keys(props.data.rates)];
+    const currencyList = [base, ...Object.keys(rates)];
     setCurrencies(currencyList);
-    setFromCurrency(props.data.base);
+    setFromCurrency(base);
     setToCurrency(currencyList[50]);
-    setExchangeRate(props.data.rates[currencyList[50]]);
-  }, []);
+    setExchangeRate(rates[currencyList[50]]);
+  }, [base, rates]);
 
   //Update the exchange rate if one of the currencies is changed
   useEffect(() => {
-    setExchangeRate(props.data.rates[toCurrency]);
-  }, [fromCurrency, toCurrency]);
+    setExchangeRate(rates[toCurrency]);
+  }, [fromCurrency, toCurrency, base, rates]);
 
   const handleFromAmountChange = (e) => {
     setAmount(e.target.value);
@@ -51,29 +54,32 @@ const CurrencyConverter = (props) => {
   return (
     <div>
       <h1 className="pageTitle">Currency Converter</h1>
-      <div className={styles.container}>
-        <h2 className={styles.heading}>Convert</h2>
-        <CurrencyRow
-          //   currencyList={currencies} Deactivated because API free plan doesn't allow you to change the base currency
-          currencyList={["EUR"]}
-          selected={fromCurrency}
-          onChangeCurrency={(e) => setFromCurrency(e.target.value)}
-          amount={fromAmount}
-          onChangeAmount={handleFromAmountChange}
-        />
-        <div className={styles.equals}>=</div>
-        <CurrencyRow
-          currencyList={currencies}
-          selected={toCurrency}
-          onChangeCurrency={(e) => setToCurrency(e.target.value)}
-          amount={toAmount}
-          onChangeAmount={handleToAmountChange}
-        />
+      {currencies && fromCurrency && toCurrency && exchangeRate && (
+        <div className={styles.container}>
+          <h2 className={styles.heading}>Convert</h2>
 
-        <h3
-          className={styles.rate}
-        >{`Current Exchange Rate: 1 ${fromCurrency} = ${exchangeRate} ${toCurrency}`}</h3>
-      </div>
+          <CurrencyRow
+            //   currencyList={currencies} Deactivated because API free plan doesn't allow you to change the base currency
+            currencyList={["EUR"]}
+            selected={fromCurrency}
+            onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+            amount={fromAmount}
+            onChangeAmount={handleFromAmountChange}
+          />
+          <div className={styles.equals}>=</div>
+          <CurrencyRow
+            currencyList={currencies}
+            selected={toCurrency}
+            onChangeCurrency={(e) => setToCurrency(e.target.value)}
+            amount={toAmount}
+            onChangeAmount={handleToAmountChange}
+          />
+
+          <h3
+            className={styles.rate}
+          >{`Current Exchange Rate: 1 ${fromCurrency} = ${exchangeRate} ${toCurrency}`}</h3>
+        </div>
+      )}
     </div>
   );
 };
